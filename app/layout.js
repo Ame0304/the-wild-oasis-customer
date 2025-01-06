6fa53eeff38a1770dc2bac8e5b5ed4ab21b72640
Ame0304 <rhj903265106@gmail.com>
2025-01-06 15:53:34 -0500

    Improve Navigation and Root layout

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ const josefin = Josefin_Sans({
 });
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   title: {
@@ -22,13 +23,12 @@ export default function Root({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-scree flex flex-col antialiased`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
