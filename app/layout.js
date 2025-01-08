35e2baed921b484a1b3d77de889b41b6c854a04e
Ame0304 <rhj903265106@gmail.com>
2025-01-08 12:31:08 -0500

    Add nested pages and layouts

--- app/layout.js
+++ app/layout.js
@@ -23,11 +23,11 @@ export default function Root({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-scree flex flex-col antialiased `}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-scree flex flex-col antialiased min-h-screen `}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
